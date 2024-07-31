1ebbd45c08b54d2c527ca2d060a14f65538a18b6
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2024-07-31 10:29:37 -0300

    aula 3107

---
+++ NetBeansIF/NetBeansProjects/UML/src/uml/Funcionario.java
@@ -0,0 +1,11 @@
+package uml;
+
+public abstract class Funcionario {
+
+    private String nome;
+
+    private String CPF;
+
+    public void relatorio() {
+    }
+}
---
+++ NetBeansIF/NetBeansProjects/UML/src/uml/Gerente.java
@@ -0,0 +1,11 @@
+package uml;
+
+public class Gerente extends Funcionario implements Autenticavel {
+
+    private int funcionariosGerenciados;
+
+    @Override
+    public void login() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
