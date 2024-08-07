1193628dc4cedc752d23a4ddab97204b990ce976
Victoria <victoriarabelocaixeta28@gmail.com>
2024-08-07 16:47:39 -0300

    tentativa de interface

---
+++ NetBeansIF/NetBeansProjects/Faculdade_VictoriaTiago/src/Faculdade/InterfaceFaculdade.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Faculdade;
+
+/**
+ *
+ * @author victo
+ */
+public interface InterfaceFaculdade {
+   
+    void listarCursos();
+
+    
+}
+    
+
--- NetBeansIF/NetBeansProjects/Faculdade_VictoriaTiago/src/Faculdade/TesteFaculdade.java
+++ NetBeansIF/NetBeansProjects/Faculdade_VictoriaTiago/src/Faculdade/TesteFaculdade.java
@@ -2,7 +2,7 @@ package Faculdade;
 
 import java.util.Scanner;
 
-public class TesteFaculdade {
+public class TesteFaculdade{
 
     public static void main(String[] args) {
 
@@ -30,22 +30,22 @@ public class TesteFaculdade {
         int idadeA = entrada.nextInt();
         System.out.println("Insira o número de matrícula:");
         String nMatricula = entrada.next();
-        
-         System.out.println("______________________________________________________");
-           
-         System.out.println("Insira a nota 1 do aluno: ");
-         double nota1 = entrada.nextDouble();
-         objAvaliacao.setNota1(nota1);
-         System.out.println("Insira a nota 2 do aluno: ");
-         double nota2 = entrada.nextDouble();
-         objAvaliacao.setNota2(nota2);
-         System.out.println("Insira a nota 3 do aluno: ");
-         double nota3 = entrada.nextDouble();
-         objAvaliacao.setNota3(nota3);
-         System.out.println("Insira a nota 4 do aluno: ");
-         double nota4 = entrada.nextDouble();
-         objAvaliacao.setNota4(nota4);
-         
+
+        System.out.println("______________________________________________________");
+
+        System.out.println("Insira a nota 1 do aluno: ");
+        double nota1 = entrada.nextDouble();
+        objAvaliacao.setNota1(nota1);
+        System.out.println("Insira a nota 2 do aluno: ");
+        double nota2 = entrada.nextDouble();
+        objAvaliacao.setNota2(nota2);
+        System.out.println("Insira a nota 3 do aluno: ");
+        double nota3 = entrada.nextDouble();
+        objAvaliacao.setNota3(nota3);
+        System.out.println("Insira a nota 4 do aluno: ");
+        double nota4 = entrada.nextDouble();
+        objAvaliacao.setNota4(nota4);
+
         System.out.println("______________________________________________________");
 
         System.out.println("Insira o nome do professor: ");
@@ -58,7 +58,7 @@ public class TesteFaculdade {
         String materiaLecionada = entrada.next();
 
         objCurso.dadosCurso(nomeCurso, objDisciplina);
-        
+
         Professor objProfessor = new Professor(nomeP, cpfP, idadeP);
         objProfessor.setMateriaLecionada(materiaLecionada);
         objProfessor.mostra();
@@ -67,7 +67,7 @@ public class TesteFaculdade {
         objAluno.setnMatricula(nMatricula);
         objAluno.mostra();
 
-
     }
-
 }
+
+    
