4f5c7b9257a62e5efd08a13e0fdd86aeee6e9222
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2024-09-04 08:29:05 -0300

    lik

---
+++ NetBeansProjects/Faculdade_VictoriaTiago/src/Faculdade/excecoes.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Faculdade;
+
+/**
+ *
+ * @author 13826640608
+ */
+public class excecoes {
+    
+    // objeto: é a instancia de uma classe. Os objetos criados sofrerão influência dos atributos e métodos a qual ele pertence
+    /*classe: é fundamental para a programação orientada em objetos pois é a classe que irá definir os objetos. Ela serve como
+    um molde para a criação de objetos, já que estes terão suas variáveis de instancia e seus métodos. Toda classe é composta de 
+    atributos e métodos.
+    */
+    
+    /* Herança: a herança permite a criação de subclasses ( correspondente à classes filhas) a partir de uma classe já existente 
+    (superclasse / classe pai). Graças a essa hierarquia, possibilitada pela palavra "extends", as classes filhas herdarão
+    os atributos e métodos que estão na classe pai. A herança também permite que o código seja reutilizado, pois é usado quando 
+    uma ou mais classes possuem atributos em comum, deixando a programação mais efetiva.
+    */
+    /* polimorfismo: o polimorfismo faz com que objetos instanciados de diferentes classes possuam o mesmo tratamento. 
+    Ou seja, se eu declaro que o objeto X da classe X é igual ao objeto y da classe Y, o objeto X usar os mesmos atributos e métodos 
+    da classe Y*/
+    /*Encapsulamento: O encapsulameto consiste em conter o código em apenas uma classe. Ele também colabora para uma 
+    melhor manutenção do sistema. Os métodos get e set fazem parte do conceito de encapsulamento, pois eles são usados
+    quando os atributos são declarados como privados. Conter o código em apenas uma classe faz com que as alterações ocorram de forma
+    mais controlada e segura.
+    */
+    /*abstração: a abstração consiste em focar em coisas que realmente são relevantes para o sistema, ocultando o que não é essencial.
+    Ela tambem permite que o código seja reutilizado e facilita a manutençao do código
+    classe abstrata: a classe abstrata não pode ser instanciada diretamente mas ela possui atributos e métodos e serve de molde para outras classes filhas.
+    As classes filhas continuarao podendo ser instanciadas.
+    interface: a intrface possui a assinatura de um método, só descreve a sua função e esse método poderá ter seus parametros/valores
+    alterados dependendo da classe em que for implementado
+    para uma classe utilizar da interface ela devera usar a palavra "implemnts".
+    */
+    /*Tratamento de excecoes: o tratamneto de excecoes obriga o sistema a rodar com sucesso mesmo que ocorra algo inesperado durante o processo de execução. 
+    Normalmente quando há um erro, na saída aparece uma mensagem de erro e o processo é interrompido
+    mas com o tratamento de excecoes, podemos exibir uma nova menagem explicando o que está incoerente de acordo com as exigencias
+    do projeto mas ele rodára as outras informacoes em seguida.
+   
+    
+    */
+}
